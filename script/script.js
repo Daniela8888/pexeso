79ef191a5a015c1536e9da7a18ed56b98985b27e
Daniela8888 <32717667+Daniela8888@users.noreply.github.com>
2017-12-03 13:47:19 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 var player=1;
+var turn1=-1;
+var turn2=-1;
 var field = [ [1,1,2,2], [3,3,4,4], [5,5,6,6], [7,7,8,8] ];
 shuffle();
 
@@ -15,20 +17,57 @@ function shuffle()
 		field[r1][c1]=field[r2][c2];
 		field[r2][c2]=temp;
 	}
-
-
 }
-
-
 function pos(value) {
+	if(turn2!=-1)
+		return;
 	var row=Math.floor(value/10);
 	var col=value%10;
 	console.log("You clicked: row:"+row+" col:"+col) ;
+	if(turn1==row*10+col)
+		return;
+
 	if(field[row][col]>0) {
 		var source="loga/img"+field[row][col]+".jpg";
 		var imageId="img"+row+col;
 		console.log(imageId) ;
 		document.getElementById(imageId).src=source;
 	}
-
+	if(turn1==-1)	//prave sme otocili 1.karticku
+		turn1=row*10+col;
+	else	{	//otocili sme 2.karticku
+		turn2=row*10+col;
+		checkSelectedCards();
+	}
 }
+
+function checkSelectedCards (){
+	var row1=Math.floor(turn1/10);
+	var row2=Math.floor(turn2/10);
+	var col1=turn1%10;
+	var col2=turn2%10;
+
+	if(field[row1][col1]==field[row2][col2]){
+
+											}
+	else{
+		setTimeout (turnBack, 1000);
+		}
+}
+
+
+function turnBack(){
+	var row1=Math.floor(turn1/10);
+	var row2=Math.floor(turn2/10);
+	var col1=turn1%10;
+	var col2=turn2%10;
+	var imageId="img"+row1+col1;
+	console.log(imageId) ;
+	document.getElementById(imageId).src="loga/back.jpg";	
+	var imageId="img"+row2+col2;
+	console.log(imageId) ;
+	document.getElementById(imageId).src="loga/back.jpg";
+	turn1=-1;
+	turn2=-1;
+}
+
